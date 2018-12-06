a16604af4fbe0cc3cb5e98ccd17cffbfbbf715d7
Andreas Reich <r_andreas2@web.de>
2018-12-06 09:05:43 +0100

    day05 part 2

--- day05/script.py
+++ day05/script.py
@@ -8,22 +8,24 @@ data = open('input.txt').read()
 
 # ----------------------------------------------------------------
 
-polymer = list(data)
-oldlen = 0
-while oldlen != len(polymer):
-    oldlen = len(polymer)
-    for pidx, letter in enumerate(polymer[1:]):
-        if letter != polymer[pidx] and letter.lower() == polymer[pidx].lower():
-            # too slow
-            #polymer = polymer[:pidx] + polymer[pidx+2:]
-            # too slow
-            #polymer.pop(pidx)
-            #polymer.pop(pidx)
-            # too slow?
-            del polymer[pidx]
-            del polymer[pidx]
-            break
-
-print('result part one:', len(polymer))
+def collapse(units):
+    polymer = [units[0]]
+    for unit in units[1:]:
+        if polymer and unit != polymer[-1] and unit.lower() == polymer[-1].lower():
+            polymer.pop()
+        else:
+            polymer.append(unit)
+    return polymer
 
 # ----------------------------------------------------------------
+
+print('result part one:', len(collapse(data)))
+
+# ----------------------------------------------------------------
+
+shortest = 9999999
+for letter in range(ord('a'), ord('z') + 1):
+    polymer = collapse(data.replace(chr(letter), '').replace(chr(letter).upper(), ''))
+    shortest = min(shortest, len(polymer))
+
+print('result part two:', shortest)
