0b045f0a2f651238ab942fbfabdef0d3429bb87e
Andreas Reich <r_andreas2@web.de>
2018-12-07 20:14:33 +0100

    day07 part 1

--- day07/script.py
+++ day07/script.py
@@ -12,20 +12,35 @@ data = [
 ]
 
 # file input    
-#data = open('input.txt').readlines()
+data = open('input.txt').readlines()
 
 # ----------------------------------------------------------------
 
 import re
 
+# parse all instructions into tuples
 instructions = []
 instr_regex = re.compile('Step ([A-Z]) must be finished before step ([A-Z]) can begin.')
 for instr in data:
     r = instr_regex.match(instr)
     instructions.append((r.group(1), r.group(2)))
 
+# determine starting active set == root nodes
+path = []
+active_set = set(instr[0] for instr in instructions if not any(cond[1] == instr[0] for cond in instructions))
 
-tree = { instr[0]:[] for instr in instructions }
+# traverse with the rule set
+while active_set:
+    cur = chr(min(ord(letter) for letter in active_set))
+    path += cur
+    active_set.remove(cur)
+    # add newly supported
+    for instr in instructions:
+        if instr[0] == cur: # just became fulfilled
+            checking = instr[1]
+            if all(cond[0] in path for cond in instructions if cond[1] == checking):
+                active_set.add(checking)
 
+print('result part one:', ''.join(path))
 
 # ----------------------------------------------------------------
