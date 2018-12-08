0f69943fecf813bd2f2d79d98b850009d2e5d8d5
Andreas Reich <r_andreas2@web.de>
2018-12-08 10:53:55 +0100

    day08

---
+++ day08/script.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+# test input
+data_raw = '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'
+
+# file input    
+data_raw = open('day08/input.txt').read()
+
+# ----------------------------------------------------------------
+
+from collections import namedtuple
+
+Node = namedtuple('Node', ['meta', 'children'])
+nodes = []
+
+data = [int(d) for d in data_raw.split(' ')]
+
+def build_tree(data_slice):
+    if not data_slice:
+        return None
+    num_children = data_slice[0]
+    num_meta = data_slice[1]
+    data_slice = data_slice[2:]
+
+    children = []
+    for _ in range(num_children):
+        data_slice, child = build_tree(data_slice)
+        children.append(child)
+    
+    node = Node(meta=data_slice[0:num_meta], children=children)
+    return data_slice[num_meta:], node
+
+_, root_node = build_tree(data)
+
+# ----------------------------------------------------------------
+
+def count_meta(node):
+    return sum(node.meta) + sum(count_meta(child) for child in node.children)
+
+print('result part one:', count_meta(root_node))
+
+# ----------------------------------------------------------------
+
+def count_special(node):
+    if not node.children:
+        return sum(node.meta)
+    return sum(count_special(node.children[meta - 1]) for meta in node.meta if meta != 0 and meta <= len(node.children))
+
+print('result part one:', count_special(root_node))
